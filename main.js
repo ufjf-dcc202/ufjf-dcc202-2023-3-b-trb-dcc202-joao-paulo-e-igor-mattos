1ece0df8f98b107517fea4ca12836cca146136d4
Joao <fernandes.joao@ufjf.br>
2023-12-10 10:32:35 -0300

    refatora operações de soma

--- main.js
+++ main.js
@@ -1,43 +1,65 @@
 const celulas = document.querySelectorAll('.cell');
-var tabuleiro = [ 
+const celulas2 = document.querySelectorAll('.cell2'); 
+var tabuleiroJ1 = [ 
 [0,0,0],
 [0,0,0],
 [0,0,0] 
 ];
+var tabuleiroJ2 = [ 
+    [0,0,0],
+    [0,0,0],
+    [0,0,0] 
+];
 
-function getTable(){
+preencheTabuleiro(tabuleiroJ1, celulas);
+preencheTabuleiro(tabuleiroJ2, celulas2);
+
+somaColuna(tabuleiroJ1);
+somaColuna(tabuleiroJ2);
+
+pontuacaoJogador(tabuleiroJ1);
+pontuacaoJogador(tabuleiroJ2);
+
+function getTable(table){
+    return table;
+}
+
+function preencheTabuleiro(table, cell){
 
     for(let i=0; i<3; i++){
         for(let j=0; j<3; j++){
-            tabuleiro[j][i] = parseInt(celulas[i * 3 + j].textContent);
+            table[j][i] = parseInt(cell[i * 3 + j].textContent);
         }
     }
-
-    return tabuleiro;
 }
 
-somaColuna();
+function somaColuna(table){
 
-function somaColuna(){
-
-    const table = getTable();
+    const jogo = getTable(table);
 
     let soma = [0, 0, 0];
     for(let j=0; j<3; j++){
-        for(let i=0; i<3; i++){
-            if(table[j][i] === table[j][i+1] && table[j][i+1] === table[j][i+2]){
-                soma[i] = table[j][i] * 3 + table[j][i+1] * 3 + table[j][i+2] * 3;
-            }
-            else if(table[j][i] === table[j][i+1]){
-                soma[i] = table[j][i] * 2 + table[j][i+1] * 2;
-            }
-            else if(table[j][i] === table[j][i+2]){
-                soma[i] = table[j][i] * 2 + table[j][i+2] * 2;
-            }
-            else if(table[j][i+1] === table[j][i+2]){
-                soma[i] = table[j][i] * 2 + table[j][i+2] * 2;
-            }
+        if(jogo[0][j] === jogo[1][j] && jogo[1][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] * 3 + jogo[1][j] * 3 + jogo[2][j] * 3;
+        }
+        else if(jogo[0][j] === jogo[1][j]){
+            soma[j] = jogo[0][j] * 2 + jogo[1][j] * 2 + jogo[2][j];
+        }
+        else if(jogo[0][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] * 2 + jogo[1][j] + jogo[2][j] * 2;
+        }
+        else if(jogo[1][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] + jogo[1][j] * 2 + jogo[2][j] * 2;
+        }
+        else{
+            soma[j] = jogo[0][j] + jogo[1][j] + jogo[2][j];
         }
     }
-    console.log(soma[0]);
+    return soma;
+}
+
+function pontuacaoJogador(table){
+
+    const colunas = somaColuna(table);
+    const total = colunas[0] + colunas[1] + colunas[2];
 }
