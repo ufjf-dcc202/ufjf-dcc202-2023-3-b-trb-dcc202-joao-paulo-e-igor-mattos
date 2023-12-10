20ff0d2f89d31614d7c94da3b6f73d88bb874981
Joao <fernandes.joao@ufjf.br>
2023-12-10 10:39:51 -0300

    separa funcoes nos scripts

--- jogada.js
+++ jogada.js
@@ -1,4 +1,50 @@
-export { escolheNumDado };
+export { getTable, escolheNumDado, atualizaTabuleiro, somaColuna, pontuacaoJogador };
+
+function getTable(table){
+    return table;
+}
+
+function atualizaTabuleiro(table, cell){
+
+    for(let i=0; i<3; i++){
+        for(let j=0; j<3; j++){
+            table[j][i] = parseInt(cell[i * 3 + j].textContent);
+        }
+    }
+}
+
+function somaColuna(table){
+
+    const jogo = getTable(table);
+
+    let soma = [0, 0, 0];
+    for(let j=0; j<3; j++){
+        if(jogo[0][j] === jogo[1][j] && jogo[1][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] * 3 + jogo[1][j] * 3 + jogo[2][j] * 3;
+        }
+        else if(jogo[0][j] === jogo[1][j]){
+            soma[j] = jogo[0][j] * 2 + jogo[1][j] * 2 + jogo[2][j];
+        }
+        else if(jogo[0][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] * 2 + jogo[1][j] + jogo[2][j] * 2;
+        }
+        else if(jogo[1][j] === jogo[2][j]){
+            soma[j] = jogo[0][j] + jogo[1][j] * 2 + jogo[2][j] * 2;
+        }
+        else{
+            soma[j] = jogo[0][j] + jogo[1][j] + jogo[2][j];
+        }
+    }
+    return soma;
+}
+
+function pontuacaoJogador(table){
+
+    const colunas = somaColuna(table);
+    const total = colunas[0] + colunas[1] + colunas[2];
+
+    console.log(total);
+}
 
 function escolheNumDado() {
     let dado = Math.floor(Math.random()* (6 - 1 + 1)) + 1;
--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import {getTable, escolheNumDado, atualizaTabuleiro, somaColuna, pontuacaoJogador} from './jogada.js';
+
 const celulas = document.querySelectorAll('.cell');
 const celulas2 = document.querySelectorAll('.cell2'); 
 var tabuleiroJ1 = [ 
@@ -11,55 +13,11 @@ var tabuleiroJ2 = [
     [0,0,0] 
 ];
 
-preencheTabuleiro(tabuleiroJ1, celulas);
-preencheTabuleiro(tabuleiroJ2, celulas2);
+atualizaTabuleiro(tabuleiroJ1, celulas);
+atualizaTabuleiro(tabuleiroJ2, celulas2);
 
 somaColuna(tabuleiroJ1);
 somaColuna(tabuleiroJ2);
 
 pontuacaoJogador(tabuleiroJ1);
 pontuacaoJogador(tabuleiroJ2);
-
-function getTable(table){
-    return table;
-}
-
-function preencheTabuleiro(table, cell){
-
-    for(let i=0; i<3; i++){
-        for(let j=0; j<3; j++){
-            table[j][i] = parseInt(cell[i * 3 + j].textContent);
-        }
-    }
-}
-
-function somaColuna(table){
-
-    const jogo = getTable(table);
-
-    let soma = [0, 0, 0];
-    for(let j=0; j<3; j++){
-        if(jogo[0][j] === jogo[1][j] && jogo[1][j] === jogo[2][j]){
-            soma[j] = jogo[0][j] * 3 + jogo[1][j] * 3 + jogo[2][j] * 3;
-        }
-        else if(jogo[0][j] === jogo[1][j]){
-            soma[j] = jogo[0][j] * 2 + jogo[1][j] * 2 + jogo[2][j];
-        }
-        else if(jogo[0][j] === jogo[2][j]){
-            soma[j] = jogo[0][j] * 2 + jogo[1][j] + jogo[2][j] * 2;
-        }
-        else if(jogo[1][j] === jogo[2][j]){
-            soma[j] = jogo[0][j] + jogo[1][j] * 2 + jogo[2][j] * 2;
-        }
-        else{
-            soma[j] = jogo[0][j] + jogo[1][j] + jogo[2][j];
-        }
-    }
-    return soma;
-}
-
-function pontuacaoJogador(table){
-
-    const colunas = somaColuna(table);
-    const total = colunas[0] + colunas[1] + colunas[2];
-}
